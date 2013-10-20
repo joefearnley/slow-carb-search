487abb6413b82277468f8b03c451a207f81fe105
Joe Fearnley <joe.fearnley@gmail.com>
2013-10-20 12:09:37 -0400

    Adding functional tests for admin controller

--- app/tests/functional/AdminControllerFunctionalTest.php
+++ app/tests/functional/AdminControllerFunctionalTest.php
@@ -33,16 +33,119 @@ class AdminControllerFunctionalTest extends TestCase {
         $food->save();
     }
 
-    public function test()
+    /**
+     * Test main login screen. Database is seeded with the following information:
+     * username: scs
+     * password: password
+     * 
+     * This test should authenticate the user, store the username, and redirect to the main admin page.
+     *
+     * @return void
+     */
+    public function testSuccessfulLogin()
     {
-        $formData = ['food' => 'AllowedFood'];
+        $formData = [
+            'username' => 'scs',
+            'password' => 'password'
+        ];
 
-        $this->call('POST', '/search', $formData);
+        $this->call('POST', '/admin/login', $formData);
 
-        $this->assertResponseOk();
-        $this->assertViewHas('food_name', 'AllowedFood');
-        $this->assertViewHas('is_isnot', ' is');
-        $this->assertViewHas('similar_food', null);
+        $this->assertRedirectedTo('admin');
+        $this->assertSessionHas('username');
     }
 
+    /**
+     * Test unsuccessful login. User should be redirect to login form with error message.
+     *
+     * @return void
+     */
+    public function testUnsuccessfulLogin()
+    {
+        $formData = [
+            'username' => 'scs',
+            'password' => 'scs'
+        ];
+
+        $this->call('POST', '/admin/login', $formData);
+
+        $this->assertRedirectedTo('admin/login');
+        $this->assertSessionHas('login_error_message');
+    }
+    
+    /**
+     * Test that foods are list on page.
+     * 
+     * @return void
+     */
+    public function testFoodsAreListedOnPage()
+    {
+        $this->call('GET', '/admin/food/list');
+        
+        $this->assertResponseOk();
+        $this->AssertViewHas('foods');
+    }
+
+    /**
+     * Edit food record and confirm data is given to the view. 
+     * Save the form and confirm data is stored correctly.
+     * 
+     * @return void
+     */
+    public function testEditAndSaveFood()
+    {
+        $this->call('GET', '/admin/food/edit/1');
+        
+        $this->assertResponseOk();
+        $this->AssertViewHas('food');
+        $this->AssertViewHas('foodGroups');
+        
+        $formData = [
+            'id' => 1,
+            'name' => 'AllowedFoodChanged',
+            'description' => 'AllowedFoodChangedDescription',
+            'allowed' => true,
+            'allowed-in-moderation' => true
+        ];
+        
+        $this->call('POST', '/admin/food/save', $formData);
+
+        $this->assertRedirectedTo('admin/food/list');
+
+        $food = Food::find(1);
+
+        $this->assertEquals($food->name, 'AllowedFoodChanged');
+        $this->assertEquals($food->description, 'AllowedFoodChangedDescription');
+    }
+    
+    /**
+     * Add food record and confirm data stored in the database correctly.
+     * 
+     * @return void
+     */
+    public function testAddFood()
+    {
+        $this->call('GET', '/admin/food/add');
+        
+        $this->assertResponseOk();
+
+        $formData = [
+            'id' => 1,
+            'name' => 'NewFood',
+            'description' => 'NewFoodDesciption',
+            'allowed' => false,
+            'allowed-in-moderation' => true
+        ];
+        
+        $this->call('POST', '/admin/food/add', $formData);
+
+        $this->assertRedirectedTo('admin/food/list');
+     /*
+        $this->assertEquals($food->name, 'NewFood');
+        $this->assertEquals($food->description, 'NewFoodDescription');
+        $this->assertFalse($food->allowed);
+        $this->assertTrue($food->allowed_moderation);
+    */
+    }
+    
 }
