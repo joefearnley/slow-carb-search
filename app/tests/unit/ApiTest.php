4e5c195e848acbc074430591e82375286aad57d0
Joe Fearnley <joe.fearnley@gmail.com>
2013-11-26 22:32:46 -0500

    Adding new test for intial API endpoint

--- app/tests/unit/ApiTest.php
+++ app/tests/unit/ApiTest.php
@@ -14,15 +14,47 @@ class ApiTest extends TestCase {
         $this->assertResponseOk();
     }
 
-    public function testSearchResults()
+    /**
+     * Test call to the search API with input with no match on a food.
+     *
+     * @return void
+     */
+    public function testSearchResultsNoFoodFound()
     {
         $response = $this->call('GET', '/api/search/chicken');
 
-        var_dump(json_decode($response->getContent()));
+        $this->assertResponseOk();
 
-        die();
+        $results = json_decode($response->getContent());
+
+        $this->assertNull($results->food);
+        $this->assertEquals('chicken', $results->searchInput);
+        $this->assertEquals(' is not allowed on the Slow Carb Diet', $results->message);
+        $this->assertNull($results->similarFoodName);
+    }
+
+    /**
+     * Test call to the search API with input with a match on a food.
+     *
+     * @return void
+     */
+    public function testSearchResultsFoodFound()
+    {
+        $response = $this->call('GET', '/api/search/AllowedFood');
 
         $this->assertResponseOk();
+
+        $results = json_decode($response->getContent());
+
+        var_dump($results);
+        die();
+
+        $this->assertTrue($results->food->allowed);
+
+        $this->assertEquals('AllowedFood', $results->searchInput);
+        $this->assertEquals(' is allowed on the Slow Carb Diet', $results->message);
+        $this->assertNull($results->similarFoodName);
     }
+
 
 }
