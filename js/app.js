ecb8ccb7a1cb2b8a48bf9364cc776d27156f4a1d
Joe Fearnley <joe.fearnley@gmail.com>
2015-01-27 22:44:36 -0500

    getting up and going with backbone

--- js/app.js
+++ js/app.js
@@ -1,36 +1,4 @@
+var app = app || {};
 
-var app = angular.module('slowcarbsearchApp', ['ngRoute', 'firebase']);
-
-app.config(function($routeProvider) {
-    $routeProvider.when('/admin', {
-        templateUrl: 'partials/admin.html',
-        controller: 'AdminController'
-    }).when('/admin/foods/list', {
-        templateUrl: 'partials/food-list.html',
-        controller: 'FoodController'
-    }).when('/admin/foods/edit/:id', {
-        templateUrl: 'partials/food-list.html',
-        controller: 'FoodController'
-    }).otherwise({
-        redirectTo: '/admin'
-    });
+app.Food = Backbone.Model.extend({
 });
-
-//app.controller('IndexController', ['$scope', '$firebase',
-//    function($scope, $firebase) {
-//        var ref = new Firebase('https://slowcarbsearch.firebaseio.com/foods');
-//        var sync = $firebase(ref);
-//        var foods = sync.$asObject();
-//
-//        $scope.search = function() {
-//            var query = angular.uppercase(this.search.input);
-//            angular.forEach(foods, function(food){
-//                if(!angular.isFunction(food)) {
-//                    if(angular.uppercase(food.name) == query) {
-//                        // food is found...is allowed
-//                    }
-//                }
-//            });
-//        }
-//    }
-//]);
